57a758b01ab917f71e96bb377eb162ecd7327bfd
Jonathan <jonathan.george.dev@gmail.com>
2020-10-28 00:18:45 -0700

    learning like a boss

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
+import Challenge1 from "./testingGrid/Callenge1";
+
 import Practice from "./testingGrid/Practice";
 
 function App() {
   return (
-    <div className="container">
+    <div>
       <Practice />
+      <Challenge1 />
     </div>
   );
 }
---
+++ src/testingGrid/Callenge1.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../style/grid.css";
+function Challenge1() {
+  return (
+    <div className="challenge-1">
+      <div className="header">Header</div>
+      <div className="box-1">Small Box 1</div>
+      <div className="box-2">Small Box 2</div>
+      <div className="box-3">Small Box 3</div>
+      <div className="sidebar">Sidebar</div>
+      <div className="main_content">Main content</div>
+      <div className="footer">Footer</div>
+    </div>
+  );
+}
+
+export default Challenge1;
