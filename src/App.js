681c449ad97ada1ad6ef9a8112ca6cde1a747ed1
Jonathan <jonathan.george.dev@gmail.com>
2020-10-29 22:53:50 -0700

    new section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import Features from "./components/Features";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Realtors from "./components/Realtors";
+import Story from "./components/Story";
 import "./sass/main.css";
 function App() {
   return (
     <div className="container">
       <Header />
+      <Realtors />
+      <section class="features">
+        <Features />
+      </section>
+      <Story />
       <Footer />
     </div>
   );
---
+++ src/components/Features.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Features() {
+  return (
+    <div class="feature">
+      <svg class="feature__icon"></svg>
+      <h4 class="heading-4 heading-4--dark">World's best luxury homes</h4>
+      <p class="feature__text">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+        distinctio necessitatibus pariatur voluptatibus.
+      </p>
+    </div>
+  );
+}
+
+export default Features;
